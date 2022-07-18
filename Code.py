59fdc36b0eec4135311040b10dd2627bf24a2f6e
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-18 11:39:47 +0530

    black when no motion, coloured when move

--- Code.py
+++ Code.py
@@ -1,7 +1,10 @@
 import cv2
 cam = cv2.VideoCapture(0)
 while cam.isOpened():
-    ret, frame = cam.read()
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1,frame2)
+
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Security Camera', frame)
+    cv2.imshow('Security Camera', diff)
