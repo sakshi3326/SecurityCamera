4f2bb28351b90d60ec46f14a141f98341c4f12ba
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-18 11:41:50 +0530

    gray while move

--- Code.py
+++ Code.py
@@ -4,7 +4,8 @@ while cam.isOpened():
     ret, frame1 = cam.read()
     ret, frame2 = cam.read()
     diff = cv2.absdiff(frame1,frame2)
+    gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
 
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Security Camera', diff)
+    cv2.imshow('Security Camera', gray)
