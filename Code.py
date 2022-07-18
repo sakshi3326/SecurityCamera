a4bc2a5a45cf5a54c9b5fad6f31f1ac0b0a933bb
sakshi3326 <saqkshiiitian123@gmail.com>
2022-07-18 11:45:58 +0530

    brighter and sharper

--- Code.py
+++ Code.py
@@ -7,6 +7,8 @@ while cam.isOpened():
     gray = cv2.cvtColor(diff, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
 
+    _, thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
+
     if cv2.waitKey(10) == ord('q'):
         break
-    cv2.imshow('Security Camera', blur)
+    cv2.imshow('Security Camera', thresh)
